0b21b3a43e0af0eaf476d52864bd0df387b091ac
MikeAdu <mike_adusah@yahoo.com>
2021-09-07 17:40:27 -0700

    added question in app js

---
+++ app.js
@@ -0,0 +1,26 @@
+var questions = [{
+    title: "What did we learn during week 1",
+    choices: ["HTML( )", "CSS( )", "JavaScript( )", "Node.JS( )"],
+    answer: "HTML( )"
+},
+{
+    title: "Which array method returns the length of an array",
+    choices: ["length( )", "git status( )", "push( )", "pop( )"],
+    answer: "length( )"
+},
+{
+    title: " What does API stand for",
+    choices: ["Apps Programs Ice cream( )", "Application Programming Interface( )", "Application Python Interface( )", "All of the above."],
+    answer: "Application Programming Interface( )"
+},
+{
+    title: "Which one is an IDE",
+    choices: ["YouTube( )", "Atom( )", "Visual Studio Code( )", "Coda( )"],
+    answer: "Visual Studio Code( )"
+},
+{
+    title: "WHich one is a JavaScript framework",
+    choices: ["React( )", "BootStrap( )", "Material Design ( )", "Material UI( )"],
+    answer: "React( )"
+}
+]
